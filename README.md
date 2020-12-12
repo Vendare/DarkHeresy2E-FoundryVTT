f9735d90abb734e153d50f195e60f13c9345676b
Russell <russellthurman94@gmail.com>
2020-12-12 13:55:58 -0600

    Added Fred's changes

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-# Dark Heresy 2E
-An **UNOFFICIAL** system for playing Dark Heresy 2E on Foundry VTT.
+# Dark Heresy 2E - Continued
+
+An **UNOFFICIAL** system for playing Dark Heresy 2E on [Foundry VTT](https://foundryvtt.com/).
+
 It provides support for **character sheets only**, game content should be drawn from official source books.
+
+The project is being continued under the GPL-3.0 License after the original author deleted their project.
 
 ## Install
 1. Go to the setup page and choose **Game Systems**.
-2. Click the **Install System** button, and paste in this [manifest link](https://raw.githubusercontent.com/Perfectro/dark-heresy-foundry-vtt/master/system.json)
+2. Click the **Install System** button, and paste in this [manifest link](system.json)
 3. Create a Game World using the Dark Heresy system.
 
 ## Preview
-![Acolyte 1](https://github.com/Perfectro/dark-heresy-foundry-vtt/blob/master/asset/preview/acolyte1.jpg?raw=true)  
-![Acolyte 2](https://github.com/Perfectro/dark-heresy-foundry-vtt/blob/master/asset/preview/acolyte2.jpg?raw=true)  
+![Acolyte 1](asset/preview/acolyte1.jpg?raw=true)  
+![Acolyte 2](asset/preview/acolyte2.jpg?raw=true)  
 
 ## Related Website
 - https://foundryvtt.com/
--- script/common/migration.js
+++ script/common/migration.js
@@ -1,5 +1,5 @@
 export const migrateWorld = async () => {
-    const schemaVersion = 1;
+    const schemaVersion = 2;
     const worldSchemaVersion = Number(game.settings.get("dark-heresy", "worldSchemaVersion"));
     if (worldSchemaVersion !== schemaVersion && game.user.isGM) {
         ui.notifications.info("Upgrading the world, please wait...");
@@ -27,6 +27,51 @@ const migrateActorData = (actor, worldSchemaVersion) => {
         if (actor.type === "acolyte" || actor.type === "npc") {
             actor.data.skills.psyniscience.characteristics = [ "Per", "WP" ];
             update["data.skills.psyniscience"] = actor.data.skills.psyniscience;
+        }
+    }
+    if (worldSchemaVersion < 2) {
+        if (actor.type === "acolyte" || actor.type === "npc") {
+
+            let characteristic = actor.data.characteristics.intelligence.base
+            let advance = -20
+            let total = characteristic.total + advance
+
+            actor.data.skills.forbiddenLore.specialities.officioAssassinorum = {
+                "label": "Officio Assassinorum",
+                "isKnown": false,
+                "advance": advance,
+                "total": total,
+                "cost": 0
+            }
+            actor.data.skills.forbiddenLore.specialities.pirates = {
+                "label": "Pirates",
+                "isKnown": false,
+                "advance": advance,
+                "total": total,
+                "cost": 0
+            }
+            actor.data.skills.forbiddenLore.specialities.psykers = {
+                "label": "Psykers",
+                "isKnown": false,
+                "advance": advance,
+                "total": total,
+                "cost": 0
+            }
+            actor.data.skills.forbiddenLore.specialities.theWarp = {
+                "label": "The Warp",
+                "isKnown": false,
+                "advance": advance,
+                "total": total,
+                "cost": 0
+            }
+            actor.data.skills.forbiddenLore.specialities.xenos = {
+                "label": "Xenos",
+                "isKnown": false,
+                "advance": advance,
+                "total": total,
+                "cost": 0
+            }
+            update["data.skills.forbiddenLore"] = actor.data.skills.forbiddenLore;
         }
     }
     return update;
