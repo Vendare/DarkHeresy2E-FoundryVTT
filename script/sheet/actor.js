754289da4d3780f0bdfed1c7fa6887ade9295220
Vindexus <vindexus@gmail.com>
2021-01-05 17:35:30 -0800

    For skill rolls added a dropown to selected a characteristic that changes target

--- script/sheet/actor.js
+++ script/sheet/actor.js
@@ -78,14 +78,32 @@ export class DarkHeresySheet extends ActorSheet {
     await prepareCommonRoll(rollData);
   }
 
+  _getCharacteristicOptions (selected) {
+    const characteristics = []
+    for (let char of Object.values(this.actor.data.data.characteristics)) {
+      characteristics.push({
+        label: char.label,
+        base: char.base,
+        selected: char.short === selected
+      })
+    }
+    return characteristics
+  }
+
   async _prepareRollSkill(event) {
     event.preventDefault();
     const skillName = $(event.currentTarget).data("skill");
     const skill = this.actor.data.data.skills[skillName];
+    const defaultChar = skill.defaultCharacteristic || skill.characteristics[0]
+
+    const characteristics = this._getCharacteristicOptions(defaultChar)
+
     const rollData = {
       name: skill.label,
       baseTarget: skill.total,
-      modifier: 0
+      modifier: 0,
+      skill: skill,
+      characteristics: characteristics
     };
     await prepareCommonRoll(rollData);
   }
