b173658dfed627deeda4f1be6ec49e7a64b4f2bd
Filippo Polo <filippo@shpsoftware.it>
2023-03-22 19:57:46 +0100

    Fixed proper editable status of psychic powers.

--- script/common/hooks.js
+++ script/common/hooks.js
@@ -115,5 +115,5 @@ Hooks.on("hotbarDrop", (bar, data, slot) => {
 
 Hooks.on("renderDarkHeresySheet", (sheet, html, data) => {
   html.find("input.cost").prop('disabled', game.settings.get("dark-heresy", "autoCalcXPCosts"));
-  html.find("input.item-cost").prop('disabled', game.settings.get("dark-heresy", "autoCalcXPCosts"));
+  html.find(":not(.psychic-power) > input.item-cost").prop('disabled', game.settings.get("dark-heresy", "autoCalcXPCosts"));
 });
